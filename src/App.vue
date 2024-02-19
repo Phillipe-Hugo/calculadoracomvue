cc2a97a62d897c669e6e6c6fea683d4c3f356ccb
Phillipe Hugo <phillipehugo1985@gmai.com>
2024-02-19 13:25:27 -0300

    Atualizar conteúdo do App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
+<style>
+@import './styles.css';
+</style>
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <label class="label1" for="numero1">Número 1:</label>
+    <input v-model="numero1" type="number" @input="calcularResultado" />
+
+    <label class="label" for="numero2">Número 2:</label>
+    <input v-model="numero2" type="number" @input="calcularResultado" />
+
+  
+
+    <label class="respotlabe" for="operacao">Operação:</label>
+    <select v-model="operacao" @change="calcularResultado">
+      <option value="soma">Soma</option>
+      <option value="subtracao">Subtração</option>
+      <option value="divisao">Divisão</option>
+      <option value="multiplicacao">Multiplicação</option>
+      <!-- Adicione mais operações conforme necessário -->
+    </select>
+
+    <p>Resultado: {{ resultado }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      numero1: 0,
+      numero2: 0,
+      operacao: 'soma',
+      resultado: 0,
+    };
+  },
+  methods: {
+    calcularResultado() {
+      switch (this.operacao) {
+        case 'soma':
+          this.resultado = this.numero1 + this.numero2;
+          break;
+        case 'subtracao':
+          this.resultado = this.numero1 - this.numero2;
+          break;
+        case 'divisao':
+          this.resultado = this.numero1 / this.numero2
+          break;
+          case 'multiplicacao':
+            if(this.numero2 != 0){
+              this.resultado = this.numero1 * this.numero2;
+            }else{
+              alert("Não é possível dividir por zero");
+            }
+            
+          break;
+        default:
+          this.resultado = 0;
+      }
+    },
+  },
+};
 </script>
 
 <style>
